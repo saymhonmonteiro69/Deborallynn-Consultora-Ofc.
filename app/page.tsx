ae5458c45f6a1c622b4a275db542201bf4c22c2f
saymhonmonteiro69 <saymhon.monteiro69@gmail.com>
2026-07-31 13:20:27 -0400

    Rename Page component to Home and update import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { TvlarLandingPage } from '@/components/tvlar-landing-page'
+import TvlarLandingPage from "@/components/tvlar-landing-page"
 
-export default function Page() {
+export default function Home() {
   return <TvlarLandingPage />
 }
